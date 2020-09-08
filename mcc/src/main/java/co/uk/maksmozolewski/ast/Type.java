3e2fa0b0fb373ab674f14f9a404167b0bc9a4afa
makspll <makspl17@gmail.com>
2020-09-08 19:32:36 +0100

    added new methods to type node

--- mcc/src/main/java/co/uk/maksmozolewski/ast/Type.java
+++ mcc/src/main/java/co/uk/maksmozolewski/ast/Type.java
@@ -3,5 +3,7 @@ package co.uk.maksmozolewski.ast;
 public interface Type extends ASTNode {
 
     public <T> T accept(ASTVisitor<T> v);
-    
+    public boolean isStructTypeType();
+    public boolean isArrayType();
+	public boolean isPointerType();
 }
