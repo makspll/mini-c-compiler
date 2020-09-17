e60b6198e29111942905c0ca6a0c175a3a7008ed
makspll <makspl17@gmail.com>
2020-09-17 03:41:38 +0100

    updated Main.java

--- mcc/src/main/java/co/uk/maksmozolewski/Main.java
+++ mcc/src/main/java/co/uk/maksmozolewski/Main.java
@@ -1,9 +1,12 @@
 package co.uk.maksmozolewski;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +21,7 @@ import co.uk.maksmozolewski.ast.PointerType;
 import co.uk.maksmozolewski.ast.Program;
 import co.uk.maksmozolewski.ast.Stmt;
 import co.uk.maksmozolewski.ast.VarDecl;
+import co.uk.maksmozolewski.gen.CodeGenerator;
 import co.uk.maksmozolewski.lexer.Scanner;
 import co.uk.maksmozolewski.lexer.Token;
 import co.uk.maksmozolewski.lexer.Tokeniser;
@@ -42,6 +46,42 @@ public class Main {
     private enum Mode {
         LEXER, PARSER, AST, SEMANTICANALYSIS, GEN
     }
+		
+    public static final List<FunDecl> stlib = new LinkedList<FunDecl>(Arrays.asList(
+        new FunDecl(BaseType.VOID,
+            "print_c",
+            new LinkedList<VarDecl>(Arrays.asList(
+            new VarDecl(BaseType.CHAR,"c"))),
+            new Block(new LinkedList<VarDecl>(),new LinkedList<Stmt>())),
+        new FunDecl(BaseType.VOID, 
+            "print_s", 
+            new LinkedList<VarDecl>(Arrays.asList(
+                new VarDecl(new PointerType(BaseType.CHAR), "s"))), 
+            new Block(new LinkedList<VarDecl>(),new LinkedList<Stmt>())
+        ),
+        new FunDecl(BaseType.VOID,
+            "print_i",
+            new LinkedList<VarDecl>(Arrays.asList(
+                new VarDecl(BaseType.INT,"i"))
+            ),
+            new Block(new LinkedList<VarDecl>(),new LinkedList<Stmt>())
+        ),
+        new FunDecl(BaseType.CHAR,
+            "read_c",
+            new LinkedList<VarDecl>(),
+            new Block(new LinkedList<VarDecl>(),new LinkedList<Stmt>())),
+        new FunDecl(BaseType.INT,
+            "read_i",
+            new LinkedList<VarDecl>(),
+            new Block(new LinkedList<VarDecl>(),new LinkedList<Stmt>())),
+        new FunDecl(new PointerType(BaseType.VOID),
+            "mcmalloc",
+            new LinkedList<VarDecl>(Arrays.asList(
+                new VarDecl(BaseType.INT, "size")
+                
+            )),
+            new Block(new LinkedList<VarDecl>(),new LinkedList<Stmt>()))
+    ));
 
     private static void usage() {
         System.out.println("Usage: java " + Main.class.getSimpleName() + " inputfile outputfile");
@@ -50,7 +90,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        if (args.length != 2)
+        if (args.length != 2 )
             usage();
 
         File inputFile = new File(args[0]);
@@ -60,32 +100,57 @@ public class Main {
         try {
             scanner = new Scanner(inputFile);
         } catch (FileNotFoundException e) {
-            System.out.println("File "+inputFile.toString()+" does not exist.");
+            System.err.println("File "+inputFile.toString()+" does not exist.");
             System.exit(FILE_NOT_FOUND);
             return;
         }
-
+        System.out.println("Compiling...");
         Tokeniser tokeniser = new Tokeniser(scanner);
         Parser parser = new Parser(tokeniser);
-        Program ast = parser.parse();
+        Program program = parser.parse();
 
-        PrintWriter writer = new PrintWriter(outputFile);
+        // write tree to outfile.astdot
+        
+        // analyze code
+        SemanticAnalyzer semAnalyser = new SemanticAnalyzer();
+        int semErrCount = semAnalyser.analyze(program);
+
+        // generate code if success
+        if(semErrCount == 0){
+            CodeGenerator codeGen = new CodeGenerator(outputFile);
+            codeGen.emitProgram(program);
+        }
+
+        System.out.println("Done with: " + (tokeniser.getErrorCount() + parser.getErrorCount() + semErrCount) +" errors.");     
+    
+        
+        // draw ast
+        File astFile = new File(args[1] + ".astdump");
+        
+        PrintWriter writer = new PrintWriter(astFile);
+
         ASTDotPrinter dotPrinter = new ASTDotPrinter(writer);
 
         try {
-            ast.accept(dotPrinter);
+            program.accept(dotPrinter);
         } catch (Exception e) {
-            
+            System.err.println("DotPrinter encountered an exception, continuing.");
         }
 
         writer.close();
-        
-
+        // draw tree to outfile.ast.png
         Runtime rt = Runtime.getRuntime();
-        Process pr = rt.exec("dot -Tpng out.txt -o program.png");
+        String dotDrawCommand = "dot -Tpng "+ args[1] + ".astdump -o " + args[1] + ".ast.png";
+        Process dotProcess = rt.exec(dotDrawCommand);
         
-        SemanticAnalyzer semAnalyser = new SemanticAnalyzer();
-        int semErrCount = semAnalyser.analyze(ast);
-        System.out.println("Done with: " + (tokeniser.getErrorCount() + parser.getErrorCount() + semErrCount) +" errors.");     
-    }
+        BufferedReader stdInput = new BufferedReader(new 
+                InputStreamReader(dotProcess.getInputStream()));
+   
+        BufferedReader stdError = new BufferedReader(new 
+                InputStreamReader(dotProcess.getErrorStream()));
+        
+        // Read the output from the command
+        System.out.println("Drawing AST tree");
+
+     }
 }
